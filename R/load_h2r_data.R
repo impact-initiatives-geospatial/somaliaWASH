92acd7786bdc16cb8d70a62cd5a2c611d53d34a0
Anthony Twesigye <twesigye10@gmail.com>
2022-10-26 11:56:40 +0300

    added function: load_h2r_data

---
+++ R/load_h2r_data.R
@@ -0,0 +1,17 @@
+#' load_h2r_data
+#' Provide the location of the main folder in our example we were reading from OneDrive, the the path to the csv file.
+#' @return
+#' @export
+#' @description function to load H2R data set - will use environment key to specify base data directory
+#' @examples
+load_h2r_data <-  function(main_folder_path = Sys.getenv("SOM_2203_WASH_DATA"), data_path = "data/202209/raw/REACH_H2R_0522-2607_data2.csv"){
+
+  # main_folder_path <-  Sys.getenv("SOM_2203_WASH_DATA")
+  # data_path <- "data/202209/raw/REACH_H2R_0522-2607_data2.csv"
+
+  h2r_data <- readr::read_csv(file.path(main_folder_path, data_path)) |>
+    mutate(latitude = parse_number(X),
+           longitude = parse_number(geometry))
+
+  return(h2r_data)
+}
